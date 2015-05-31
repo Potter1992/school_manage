9dcb4d7abfa60f481c26c1f577da2294b9476cb0
Potter1992 <742865703@qq.com>
2015-05-31 11:42:52 +0800

    提交一次

---
+++ src/com/school/model/Apply_approve.java
@@ -0,0 +1,52 @@
+package com.school.model;
+
+import java.util.List;
+
+import com.jfinal.plugin.activerecord.Db;
+import com.jfinal.plugin.activerecord.DbPro;
+import com.jfinal.plugin.activerecord.Model;
+import com.jfinal.plugin.activerecord.Record;
+
+public class Apply_approve extends Model<Apply_approve> {
+	/**
+	 * 
+	 */
+	public static final String TableName = "apply_approve";
+	public static final Apply_approve me = new Apply_approve();
+
+	/**
+	 * 通过学号获得审核申请记录,以便学生查看
+	 * 
+	 * @param s_no
+	 * @return
+	 */
+	public List<Apply_approve> findByS_no(String s_no) {
+		return find("select * from " + TableName + " where s_no = " + s_no);
+	}
+
+	/**
+	 * 通过record保存申请审核
+	 * 
+	 * @param record
+	 * @return
+	 */
+	public boolean saveByRecordorUpdate(Record record) {
+		String s_noString=record.get("s_no");
+		String result=Db.queryStr("select s_no from " + TableName + " where  s_no = ?",
+				s_noString) ;
+		if (result!=null) {
+			if (Db.update(TableName, "s_no", record)) {
+				return true;
+			} else {
+				return false;
+			}
+		}else {
+			if (Db.save(TableName, record)) {
+				return true;
+			} else {
+				return false;
+			}
+		}
+
+	}
+}
---
+++ src/com/school/model/Role_change.java
@@ -0,0 +1,22 @@
+package com.school.model;
+
+import java.util.List;
+
+import com.jfinal.plugin.activerecord.Model;
+
+public class Role_change extends Model<Role_change> {
+	/**
+	 * 角色异动关系表
+	 */
+	public static final String TableName = "role_change";
+	public static final Role_change me = new Role_change();
+
+	public List<Role_change> findByC_id(int c_id) {
+		List<Role_change> list = find("select * from " + TableName
+				+ " where c_id = ?", c_id);
+		if (list.size()==0) {
+			return null;
+		}
+		return list;
+	}
+}
